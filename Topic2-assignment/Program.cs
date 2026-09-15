8fea4204044cb54e6afcdf03fccf68a1c48be0db
SAMMY <326589150+sqn10@users.noreply.github.com>
2026-09-15 12:51:01 -0400

    Done.

--- Topic2-assignment/Program.cs
+++ Topic2-assignment/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Sammy
+            Console.WriteLine("My birth year: " + (2026 - 16));
+            Console.WriteLine();
+            Console.WriteLine("Average of 14, 15, and 16: " + ((14 + 15 + 16) / 3));
+            Console.WriteLine();
+            Console.WriteLine("Average of 14, 15, 16, and 17: " + ((14 + 15 + 16 + 17) / 4.0));
+            Console.WriteLine();
+            Console.WriteLine("The area of a circle with a radius of 3 centimeters: " + Math.Round(3.141592653589 * (3 * 3), 2));
+            Console.WriteLine();
+            Console.WriteLine("C# knows when to add based on if you have text on one side of the operator or not.");
+            Console.WriteLine("If you want math done with no such errors you must type +. Console.WriteLine(``random text`` + (2 + 2))");
+            Console.WriteLine("If you didn't type the brackets, you'd get something like: random text2.");
         }
     }
 }
